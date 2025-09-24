ef822d6c855bdf49fd268a9c22d75e4641e951f7
ClauKev <kevinxvdp@gmail.com>
2025-09-23 22:50:26 -0300

    fix: minor fixes and adjustments

--- app/src/main/java/com/example/parcial_1_pm_acn4av_gonzales_oturakdjian/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_pm_acn4av_gonzales_oturakdjian/MainActivity.java
@@ -1,17 +1,3 @@
-package com.example.parcial_1_pm_acn4av_gonzales_oturakdjian;
-
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
